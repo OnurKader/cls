e96f375cae1056cd89ba142c2b0427b200e9283d
OnurKader <onurorkunkader1999@gmail.com>
2019-10-16 15:23:01 +0100

    README
    Added a Makefile
    
    Updated Makefile
    
    Added the vc_vector class from "https://github.com/skogorev/vc_vector"
    
    Updated Makefile to use vc_vector
    
    Realised I don't need vectors, just gonna use an array
    
    Squash these unnecessary changes
    But Added TODO's
    
    Updated README

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
-# My `list` program written in C. 'cls' = Color-ls | C-ls, 'cause it's written in C.
+# My `list` program written in C
+# 'cls' = Color-ls | C-ls, 'cause it's written in C.
+
 
 ## Screenshots
-
+___
 ## Build & Install
+Run `make` in the `src/` directory.
+To Install run `make install`, it installs in `bin/` and links to `/usr/bin`
+
 
---
+++ src/main.c
@@ -0,0 +1,166 @@
+#include <dirent.h>
+#include <getopt.h>
+#include <grp.h>
+#include <pwd.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/ioctl.h>
+#include <sys/stat.h>
+#include <time.h>
+#include <unistd.h>
+
+void usage(void);
+
+static int b_all = false, b_long = false, b_human = false, b_color = true,
+		   b_reverse = false;
+
+typedef struct
+{
+	char* name;
+	size_t size;
+} File;
+
+int main(int argc, char** argv)
+{
+	// TODO finish File struct which wraps dirent and stat / lstat(), modify time stuff,
+	// owners...
+	// TODO add getopt loop to get the dir, if no string found it'll be '.'. args are
+	// probably -alhCHv --help --version --usage -? (if possible). (C : colorless?)
+	// TODO add an external Color class
+	// TODO Terminal checks, 256 Colors.
+	// TODO Kibi-byte for sizes. Think of a better way to determine 'KGB' rather than an
+	// intense ternary
+	// TODO -l option, long listing
+	// TODO Figure out icons, just a simple map should be fine, or maybe an array but that
+	// wouldn't work nvm...
+	// TODO Reverse the array
+
+	struct dirent** dirs;
+	char* dir = ".";
+	int n_of_dirs;
+
+	/* Flags:
+		all = show dotfiles except for '.' & '..'
+
+		long = long listing, ownership, permissions, modify time, size
+
+		human_readable = Shorten sizes to Metric system, 'KGBM'.
+
+		color = by default on, colorful version. Maybe set it in color struct? Or functions
+		for color so it returns an empty string
+
+		reverse = reverse printing order, currently -r but for Recursive Tree printing in
+		the future maybe change to -R
+	*/
+	// Get the options and set the necessary flags
+	opterr = 0;
+	{
+		int opt;
+		static struct option long_options[] = {{"all", no_argument, &b_all, 1},
+											   {"long", no_argument, &b_long, 1},
+											   {"human", no_argument, &b_human, 1},
+											   {"no-color", no_argument, &b_color, 0},
+											   {"version", no_argument, 0, 'v'},
+											   {"help", no_argument, 0, 'H'},
+											   {"usage", no_argument, 0, 'u'},
+											   {"reverse", no_argument, 0, 'r'},
+											   {0, 0, 0, 0}};
+		while(true)
+		{
+			int opt_index = 0;
+			opt = getopt_long(argc, argv, "alhCHvur", long_options, &opt_index);
+			if(opt == -1)
+				break;
+
+			switch(opt)
+			{
+				case 'v':
+				case 'H':
+				case 'u': usage(); break;
+
+				case 0:
+					if(long_options[opt_index].flag != 0)
+						break;
+					printf("option %s\n", long_options[opt_index].name);
+				case 'a': b_all = 1; break;
+				case 'l': b_long = 1; break;
+				case 'h': b_human = 1; break;
+				case 'C': b_color = 0; break;
+				case 'r': b_reverse = 1; break;
+				case '?': break;
+				default: abort();
+			}
+		}
+	}
+
+	if(optind < argc)
+		dir = argv[optind++];
+
+	n_of_dirs = scandir(dir, &dirs, NULL, alphasort);
+	if(n_of_dirs < 0)
+	{
+		perror("scandir: ");
+		return 1;
+	}
+
+	File v_dirs[n_of_dirs];
+	uint16_t num_of_files = 0U;
+
+	while(n_of_dirs--)
+	{
+		char* name = dirs[n_of_dirs]->d_name;
+
+		if(!strcmp(name, ".") || !strcmp(name, ".."))
+		{
+			free(dirs[n_of_dirs]);
+			continue;
+		}
+
+		if(b_all)
+		{
+			if(name[0] == '.')
+			{
+				v_dirs[n_of_dirs - 1].name = name;
+				++num_of_files;
+			}
+			else
+			{
+				free(dirs[n_of_dirs]);
+				continue;
+			}
+		}
+		else
+		{
+			v_dirs[n_of_dirs - 1].name = name;
+			++num_of_files;
+		}
+
+		// TODO Resize the v_dirs array at the end of the while loop. maybe keep a variable
+		// of
+		// the size.
+		/* printf("Type: %d\n", */
+		/* dirs[n_of_dirs]->d_type);	// Directory = 4, Regular File = 8, Symlink = 10 */
+		// Currently prints in reverse
+		free(dirs[n_of_dirs]);
+	}
+	num_of_files -= 2;	  // Get rid of . and ..
+
+	for(int i = 0; i < num_of_files; ++i)
+	{
+		printf("%s\n", v_dirs[i].name);
+	}
+
+	printf("No. of files:%d\n", num_of_files);
+
+	free(dirs);
+	return 0;
+}
+
+void usage(void)
+{
+	printf("Usage:\n");
+	exit(1);
+}
